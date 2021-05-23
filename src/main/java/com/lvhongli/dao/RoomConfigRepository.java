02788b830031208e582e98bb0c0336a9dabb8d4d
lvhonglii <1556282924@qq.com>
2021-05-23 15:52:23 +0800

    数据库表更新

--- src/main/java/com/lvhongli/dao/RoomConfigRepository.java
+++ src/main/java/com/lvhongli/dao/RoomConfigRepository.java
@@ -1,7 +1,7 @@
 package com.lvhongli.dao;
 
 import com.lvhongli.model.RoomConfig;
-import com.lvhongli.pojo.RoomConfigEnum;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
